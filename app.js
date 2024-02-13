0d853bdeb94f5fe42694289ed4107bf759257856
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-02-13 18:35:06 +0300

    Initial setup

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const createError = require('http-errors');
+
+const app = express();
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.DB_URI, { dbName: process.env.DB_NAME })
+  .catch((err) => console.error(err));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use((req, res, next) => next(createError(404, 'Page Not Found')));
+app.use(
+  /**
+   * @param {import('http-errors').HttpError} err
+   * @param {import('express').Request} req
+   * @param {import('express').Response} res
+   * @param {import('express').NextFunction} next
+   */
+  (err, req, res, next) => {
+    const isDevEnv = process.env.NODE_ENV === 'development';
+
+    const status =
+      err.status < 500 || isDevEnv
+        ? err.status || 500
+        : 500;
+
+    const message =
+      status < 500 || isDevEnv
+        ? err.message || 'Unknown Error'
+        : 'Server Error';
+
+    const error = { status, message };
+    isDevEnv && (error.stack = err.stack);
+
+    res.status(status).json({ error });
+  }
+);
+
+const port = +process.env.PORT || 3000;
+app.listen(port, () => console.log(`App is available on port ${port}`));
