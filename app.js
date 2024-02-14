4a8a15e75afa5629deaf505b8697404ee368ffd3
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-02-14 14:45:46 +0300

    Add cors, cookie-session and connect-mongo

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
 const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
 const logger = require('morgan');
 const createError = require('http-errors');
 
@@ -11,12 +13,24 @@ mongoose
   .connect(process.env.DB_URI, { dbName: process.env.DB_NAME })
   .catch((err) => console.error(err));
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SECRET,
+    cookie: { maxAge: 1000 * 60 * 60 },
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+      client: mongoose.connection.getClient(),
+      dbName: process.env.DB_NAME
+    })
+  })
+);
 
-app.use((req, res, next) => next(createError(404, 'Page Not Found')));
+app.use((req, res, next) => next(createError(404, 'Not Found')));
 app.use(
   /**
    * @param {import('http-errors').HttpError} err
