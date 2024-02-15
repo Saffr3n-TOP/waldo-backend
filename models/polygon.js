d36bba96470af480adda959b8257c9c14755bd87
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-02-15 21:44:47 +0300

    Edit polygon schema for better intellisense

--- models/polygon.js
+++ models/polygon.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-const PolygonSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    vertices: [[{ type: Number }]]
-  },
-  { collection: 'polygons' }
-);
+const PolygonSchema =
+  new /** @type {typeof mongoose.Schema<{name: string, vertices: Polygon}>} */ (
+    mongoose.Schema
+  )(
+    {
+      name: { type: String, required: true },
+      vertices: [[Number]]
+    },
+    { collection: 'polygons' }
+  );
 
 module.exports = mongoose.model('Polygon', PolygonSchema);
